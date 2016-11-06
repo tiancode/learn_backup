2e19690b29739d4fe4fe8c880e1afcb27a61ca47
tianshuai <tianshuai@tianshuais-MacBook-Air.local>
2016-11-06 14:42:02 +0800

    knn

---
+++ breast_cancer.py
@@ -0,0 +1,32 @@
+# dataset : https://archive.ics.uci.edu/ml/
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data')
+#print(df.head())
+
+#print(df.shape)
+df.replace('?', np.nan, inplace=True)  # -99999
+df.dropna(inplace=True)
+#print(df.shape)
+
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+Y = np.array(df['class'])
+
+X_trian,X_test,Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2)
+
+#clf = LinearRegression()
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_trian, Y_train)
+
+accuracy = clf.score(X_test, Y_test)
+
+print(accuracy)
+
+sample = np.array([4,2,1,1,1,2,3,2,1])
+print(sample.reshape(1, -1))   # X.reshape(-1, 1)
+print(clf.predict(sample.reshape(1, -1)))
